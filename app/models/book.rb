3608ed1794ad55b6fa378ea1c83e0e87d56cc391
root <root@localhost>
2022-02-14 01:52:12 +0000

    Lab 3 multi book completed

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,5 @@ class Book < ApplicationRecord
     validates :author, presence: true, format: { with: /[^( )][0-9a-zA-Z( )]+[^( )]/, message: "letters and in-between spaces only" }
     validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
     validates :published_date, presence: true
+    has_many :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  resources :books
-  root 'books#index'
+  resources :ids
+  resources :users
+  resources :books do 
+    member do
+        get :delete
+    end
+
+  end
+  root 'ids#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
